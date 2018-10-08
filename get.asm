cd3aeb6a5fc4afd144ff60a86c4ee85f6e5ddfdb
GriffinMB <byatt.griffin@gmail.com>
2018-10-08 00:11:33 -0500

    Fix sublimes tab/spaces problems.

--- get.asm
+++ get.asm
@@ -12,64 +12,64 @@ section .text
 global _main
 
 _main:
-		push 		rbp
-		mov 		rbp, rsp
-		sub 		rsp, 0x50
-		mov 		r13, [rsi+8]
-		mov 		qword [rbp-8], rdi
-		mov 		qword [rbp-16], r13
-		cmp 		qword [rbp-8], 0x2
-		jne 		error
+        push 		rbp
+        mov 		rbp, rsp
+        sub 		rsp, 0x50
+        mov 		r13, [rsi+8]
+        mov 		qword [rbp-8], rdi
+        mov 		qword [rbp-16], r13
+        cmp 		qword [rbp-8], 0x2
+        jne 		error
 
-		; CURL *curl_easy_init();
-		call 		_curl_easy_init
-		mov 		r12, rax
+        ; CURL *curl_easy_init();
+        call 		_curl_easy_init
+        mov 		r12, rax
 
-		; CURLcode curl_easy_setopt(CURL *handle, CURLoption option, parameter);
-		mov 		rdi, r12
-		mov 		rsi, CURLOPT_URL
-		mov 		rdx, [rbp-16]
-		call 		_curl_easy_setopt
+        ; CURLcode curl_easy_setopt(CURL *handle, CURLoption option, parameter);
+        mov 		rdi, r12
+        mov 		rsi, CURLOPT_URL
+        mov 		rdx, [rbp-16]
+        call 		_curl_easy_setopt
 
-		mov 		rdi, r12
-		mov 		rsi, CURLOPT_WRITEFUNCTION
-		lea 		rdx, [rel pwrite]
-		call 		_curl_easy_setopt
+        mov 		rdi, r12
+        mov 		rsi, CURLOPT_WRITEFUNCTION
+        lea 		rdx, [rel pwrite]
+        call 		_curl_easy_setopt
 
-		lea 		rdi, [rel filename]
-		lea 		rsi, [rel write_perm]
-		call 		_fopen
+        lea 		rdi, [rel filename]
+        lea 		rsi, [rel write_perm]
+        call 		_fopen
         test        rax, rax
-		jz 			error
-		mov 		r13, rax
+        jz 			error
+        mov 		r13, rax
 
-		mov 		rdi, r12
-		mov 		rsi, CURLOPT_WRITEDATA
-		mov 		rdx, r13
-		call 		_curl_easy_setopt
+        mov 		rdi, r12
+        mov 		rsi, CURLOPT_WRITEDATA
+        mov 		rdx, r13
+        call 		_curl_easy_setopt
 
-		; CURLcode curl_easy_perform(CURL * easy_handle );
-		mov 		rdi, r12
-		call 		_curl_easy_perform
-		cmp 		rax, CURL_OK
-		jne 		error
-		jmp 		end
+        ; CURLcode curl_easy_perform(CURL * easy_handle );
+        mov 		rdi, r12
+        call 		_curl_easy_perform
+        cmp 		rax, CURL_OK
+        jne 		error
+        jmp 		end
 error:
-		lea 		rdi, [rel print_err]
-		call 		_printf
+        lea 		rdi, [rel print_err]
+        call 		_printf
 end:
-		; void curl_easy_cleanup(CURL *handle);
-		mov 		rdi, r12
-		call 		_curl_easy_cleanup
-		pop 		rbx
-		add 		rsp, 0x50
-		ret
+        ; void curl_easy_cleanup(CURL *handle);
+        mov 		rdi, r12
+        call 		_curl_easy_cleanup
+        pop 		rbx
+        add 		rsp, 0x50
+        ret
 
 pwrite:
-		push 		rbp
-		call 		_fwrite
-		pop 		rbp
-		ret
+        push 		rbp
+        call 		_fwrite
+        pop 		rbp
+        ret
 
 
 section .data
@@ -83,7 +83,7 @@ write_perm:
 
 
 ; curl constants
-CURL_OK equ					0
-CURLOPT_URL equ 			10002
-CURLOPT_WRITEFUNCTION equ 	20011
-CURLOPT_WRITEDATA equ		10001
+CURL_OK equ                 0
+CURLOPT_URL equ             10002
+CURLOPT_WRITEFUNCTION equ   20011
+CURLOPT_WRITEDATA equ       10001
