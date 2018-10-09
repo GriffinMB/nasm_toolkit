62ec0539ca02d9b1654ff48ae4700f987a032878
GriffinMB <byatt.griffin@gmail.com>
2018-10-08 20:19:35 -0500

    Fix the rest of sublime's tab/space issues.

--- get.asm
+++ get.asm
@@ -12,74 +12,74 @@ section .text
 global _main
 
 _main:
-        push 		rbp
-        mov 		rbp, rsp
-        sub 		rsp, 0x50
-        mov 		r13, [rsi+8]
-        mov 		qword [rbp-8], rdi
-        mov 		qword [rbp-16], r13
-        cmp 		qword [rbp-8], 0x2
-        jne 		error
+        push        rbp
+        mov         rbp, rsp
+        sub         rsp, 0x50
+        mov         r13, [rsi+8]
+        mov         qword [rbp-8], rdi
+        mov         qword [rbp-16], r13
+        cmp         qword [rbp-8], 0x2
+        jne         error
 
         ; CURL *curl_easy_init();
         call 		_curl_easy_init
         mov 		r12, rax
 
         ; CURLcode curl_easy_setopt(CURL *handle, CURLoption option, parameter);
-        mov 		rdi, r12
-        mov 		rsi, CURLOPT_URL
-        mov 		rdx, [rbp-16]
-        call 		_curl_easy_setopt
+        movd        rdi, r12
+        movd        rsi, CURLOPT_URL
+        movd        rdx, [rbp-16]
+        calld       _curl_easy_setopt
 
-        mov 		rdi, r12
-        mov 		rsi, CURLOPT_WRITEFUNCTION
-        lea 		rdx, [rel pwrite]
-        call 		_curl_easy_setopt
+        movd        rdi, r12
+        movd        rsi, CURLOPT_WRITEFUNCTION
+        lead        rdx, [rel pwrite]
+        calld       _curl_easy_setopt
 
-        lea 		rdi, [rel filename]
-        lea 		rsi, [rel write_perm]
-        call 		_fopen
-        test        rax, rax
-        jz 			error
-        mov 		r13, rax
+        lead        rdi, [rel filename]
+        lead        rsi, [rel write_perm]
+        calld       _fopen
+        testd       rax, rax
+        jzd         error
+        movd        r13, rax
 
-        mov 		rdi, r12
-        mov 		rsi, CURLOPT_WRITEDATA
-        mov 		rdx, r13
-        call 		_curl_easy_setopt
+        movd        rdi, r12
+        movd        rsi, CURLOPT_WRITEDATA
+        movd        rdx, r13
+        calld       _curl_easy_setopt
 
         ; CURLcode curl_easy_perform(CURL * easy_handle );
-        mov 		rdi, r12
-        call 		_curl_easy_perform
-        cmp 		rax, CURL_OK
-        jne 		error
-        jmp 		end
+        mov         rdi, r12
+        call        _curl_easy_perform
+        cmp         rax, CURL_OK
+        jne         error
+        jmp         end
 error:
-        lea 		rdi, [rel print_err]
-        call 		_printf
+        lea         rdi, [rel print_err]
+        call        _printf
 end:
         ; void curl_easy_cleanup(CURL *handle);
-        mov 		rdi, r12
-        call 		_curl_easy_cleanup
-        pop 		rbx
-        add 		rsp, 0x50
+        mov         rdi, r12
+        call        _curl_easy_cleanup
+        pop         rbx
+        add         rsp, 0x50
         ret
 
 pwrite:
-        push 		rbp
-        call 		_fwrite
-        pop 		rbp
+        push        rbp
+        call        _fwrite
+        pop         rbp
         ret
 
 
 section .data
 
 print_err:
-		db 			"An error occurred.", 10, 0
+		db          "An error occurred.", 10, 0
 filename:
-		db 			"filename.txt", 0
+		db          "filename.txt", 0
 write_perm:
-		db 			"wb", 0
+		db          "wb", 0
 
 
 ; curl constants
